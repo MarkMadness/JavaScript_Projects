45cf123b8088f797b6fca97621ee9d96cf79e951
Mark <1187productions@gmail.com>
2019-08-12 12:24:36 -0600

    Delete Basic_JavaScript_1.js

--- Basic JavaScript Projects/JS/Basic_JavaScript_1.js
+++
@@ -1,7 +0,0 @@
-
-var B = "Say the word, \"Hello!\"" + " and be addressed!" 
-var B = B.fontcolor("blue");
-document.write(B);
-document.write(" Let us come together "
-+ "and make a difference!")
-document.write(3+3)
