9b44dd3143617dc85ecd1d6b98b1b5ec967f2f47
Mark <1187productions@gmail.com>
2019-08-12 12:23:31 -0600

    Uploaded Basic JavaScript Projects repository

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+
+var B = "Say the word, \"Hello!\"" + " and be addressed!" 
+var B = B.fontcolor("blue");
+document.write(B);
+document.write(" Let us come together "
++ "and make a difference!")
+document.write(3+3)
