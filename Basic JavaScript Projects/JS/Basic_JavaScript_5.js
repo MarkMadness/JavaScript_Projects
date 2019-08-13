8fef955cd6956d7c53375c1b4d4b0e99d35d417c
Mark <1187productions@gmail.com>
2019-08-13 15:32:12 -0600

    Uploading JavaScript assignment 5

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_5.js
@@ -0,0 +1,23 @@
+function DataType() {
+    document.getElementById("Typeof").innerHTML = ((10-5) == 5); 
+    
+    document.write(3E310);
+
+    document.write(7>3);
+    document.write(7<3);
+
+    console.log(2+2);
+
+    document.write("10" +5);
+
+    document.write(12>43);
+
+    document.write(5===5);
+    document.write("5"===6);
+    document.write("5"===5);
+    document.write(4===5) 
+
+    document.write(5>4 && 6>4);
+    document.write(10<5 || 5>10);
+    document.write(!(5>8));
+}
