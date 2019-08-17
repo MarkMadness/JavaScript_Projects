8214038e313a111dc1efd7339d416d0b269098e1
Mark <1187productions@gmail.com>
2019-08-16 18:49:08 -0600

    Uploading final version of JavaScript assignment 7

--- Basic JavaScript Projects/JS/Basic_JavaScript_7.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_7.js
@@ -1,4 +1,4 @@
-/*
+
 var X = 5;
 function AddOne() {
     document.write(12 + X);
@@ -9,14 +9,28 @@ function AddTwo() {
 }
 AddOne();
 AddTwo(); 
-*/
 
 function IfElseTest() {
-    Power_Level = document.getElementById("Power_Level").value;
+    var Power_Level = document.getElementById("Power_Level").value;
     if(Power_Level > 9000) {
         document.getElementById("IfTest").innerHTML = "You are strong enough to face the boss."
     }
     else {
         document.getElementById("IfTest").innerHTML = "You are not strong enough to face the boss. Get better!"
     }
+}
+
+function Time_function() {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It is morning!";
+    }
+    else if (Time > 12 == Time < 18) {
+        Reply = "It is afternoon!";
+    }
+    else {
+        Reply = "It is evening!";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
 }
